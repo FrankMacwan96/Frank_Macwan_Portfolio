a66ed3c7943464a459ae1b0ff89326b069fd1eb4
Frank <macwanfrank96@gmail.com>
2020-09-12 20:05:00 -0400

    Updates

---
+++ precache-manifest.bca70374c6fec059459bbfc0a70bfbcb.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f35768399eaf0aa50bb8fc3dbcfd75",
+    "url": "/Frank_Macwan_Portfolio/index.html"
+  },
+  {
+    "revision": "26593efb037452ca6a98",
+    "url": "/Frank_Macwan_Portfolio/static/css/2.7b1f77b4.chunk.css"
+  },
+  {
+    "revision": "c0a760b7650a121d9d26",
+    "url": "/Frank_Macwan_Portfolio/static/css/main.67686863.chunk.css"
+  },
+  {
+    "revision": "26593efb037452ca6a98",
+    "url": "/Frank_Macwan_Portfolio/static/js/2.28461577.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Frank_Macwan_Portfolio/static/js/2.28461577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a760b7650a121d9d26",
+    "url": "/Frank_Macwan_Portfolio/static/js/main.73f8e11d.chunk.js"
+  },
+  {
+    "revision": "7f5f266ee5800d44e837",
+    "url": "/Frank_Macwan_Portfolio/static/js/runtime-main.b7046ee3.js"
+  },
+  {
+    "revision": "3d45f423d62a5020c05c928ad8772391",
+    "url": "/Frank_Macwan_Portfolio/static/media/arrow.3d45f423.svg"
+  },
+  {
+    "revision": "a74212b632dc205e2bd832acaa46a2e9",
+    "url": "/Frank_Macwan_Portfolio/static/media/arrow.a74212b6.svg"
+  },
+  {
+    "revision": "04e4f259c38bea403acb2148d9f90aa0",
+    "url": "/Frank_Macwan_Portfolio/static/media/contact-info-icon1.04e4f259.svg"
+  },
+  {
+    "revision": "df4fdedf2f261b6bd88551a4f1ab92f9",
+    "url": "/Frank_Macwan_Portfolio/static/media/contact-info-icon2.df4fdedf.svg"
+  },
+  {
+    "revision": "3cf93bcfa4d4d754bc4675b37bc0361b",
+    "url": "/Frank_Macwan_Portfolio/static/media/contact-info-icon3.3cf93bcf.svg"
+  },
+  {
+    "revision": "db57da795c9c521b14cf9f487da06d05",
+    "url": "/Frank_Macwan_Portfolio/static/media/dribble.db57da79.svg"
+  },
+  {
+    "revision": "aa1e8c9c8592f04d2d3d68c86a746696",
+    "url": "/Frank_Macwan_Portfolio/static/media/facebook.aa1e8c9c.svg"
+  },
+  {
+    "revision": "789bce482c497ca5006b97bfe6f0f91a",
+    "url": "/Frank_Macwan_Portfolio/static/media/logo-yellow.789bce48.svg"
+  },
+  {
+    "revision": "166136cdfdad6ba392ae1d7650bf2da9",
+    "url": "/Frank_Macwan_Portfolio/static/media/logo.166136cd.svg"
+  },
+  {
+    "revision": "39c5f3c6bdad4789fa38e3975946fe3f",
+    "url": "/Frank_Macwan_Portfolio/static/media/logo.39c5f3c6.svg"
+  },
+  {
+    "revision": "f521cf4553d6d240c47e135be9ba7a9c",
+    "url": "/Frank_Macwan_Portfolio/static/media/mobile-close.f521cf45.svg"
+  },
+  {
+    "revision": "10823520de33dffb05e6963a0d8c7bec",
+    "url": "/Frank_Macwan_Portfolio/static/media/mobile-menu.10823520.svg"
+  },
+  {
+    "revision": "1f6de9f03ce7d4129313827d29a875d1",
+    "url": "/Frank_Macwan_Portfolio/static/media/partner01.1f6de9f0.svg"
+  },
+  {
+    "revision": "d8f2a498f49a22e58484e1be708fbcf0",
+    "url": "/Frank_Macwan_Portfolio/static/media/partner02.d8f2a498.svg"
+  },
+  {
+    "revision": "0d091731c341987d1a26f18053352817",
+    "url": "/Frank_Macwan_Portfolio/static/media/partner03.0d091731.svg"
+  },
+  {
+    "revision": "81292e3cc0ec62e529d83a0df681f975",
+    "url": "/Frank_Macwan_Portfolio/static/media/partner04.81292e3c.svg"
+  },
+  {
+    "revision": "d39e28a33d995a61c2338ac40eca3c67",
+    "url": "/Frank_Macwan_Portfolio/static/media/partner05.d39e28a3.svg"
+  },
+  {
+    "revision": "a0c63ad5babbf86025666de007e4af70",
+    "url": "/Frank_Macwan_Portfolio/static/media/partner06.a0c63ad5.svg"
+  },
+  {
+    "revision": "0ecdf21ae145f462c5258daad33c2dda",
+    "url": "/Frank_Macwan_Portfolio/static/media/twitter.0ecdf21a.svg"
+  }
+]);
